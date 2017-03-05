012724bb8990ce0c366f138cae20e14fee69661a
Uli Kusterer <ulivc@zathras.de>
2017-03-05 13:43:19 +0100

    Initial Commit

---
+++ ULIGUIPluginHost/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ULIGUIPluginHost
+//
+//  Created by Uli Kusterer on 05/03/2017.
+//  Copyright © 2017 Uli Kusterer. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ ULIGUIPluginHost/AppDelegate.m
@@ -0,0 +1,28 @@
+//
+//  AppDelegate.m
+//  ULIGUIPluginHost
+//
+//  Created by Uli Kusterer on 05/03/2017.
+//  Copyright © 2017 Uli Kusterer. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@property (weak) IBOutlet NSWindow *window;
+@end
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+	// Insert code here to initialize your application
+}
+
+
+- (void)applicationWillTerminate:(NSNotification *)aNotification {
+	// Insert code here to tear down your application
+}
+
+
+@end
